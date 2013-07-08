70e534079a4ce43b79b116760a1c32dbbbcca529
Chuck Durfee <neontapir@gmail.com>
2013-07-08 10:42:55 -0600

    Revert "Revert "added 403 error handling""
    This reverts commit e31a1f15af57a596a6963b429e48cbd268955386.

--- TeamCityBot.cs
+++ TeamCityBot.cs
@@ -12,14 +12,20 @@ namespace HipChatBot
             HipChatBot bot = new TeamCityBot();
 
             string testRoom = "232822";
+            string previousRequest = string.Empty;
 
-            //while (true)
-            //{
-                string lastMessage = bot.getLastMessage(testRoom);
-                Console.WriteLine(lastMessage);
+            while (true)
+            {
+                KeyValuePair<string, string> lastMessage = bot.getLastMessage(testRoom);
+                if (lastMessage.Value.Contains("@teamcitybot") && !lastMessage.Value.Equals(previousRequest))
+                {
+                    Console.WriteLine("{0}:{1}",lastMessage.Key,lastMessage.Value);
+                    bot.sendMessage(testRoom, "teamcitybot", string.Format("Hello {0}", lastMessage.Key));
+                }
 
+                previousRequest = lastMessage.Value;
                 System.Threading.Thread.Sleep(1000);
-            //}
+            }
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadLine();
